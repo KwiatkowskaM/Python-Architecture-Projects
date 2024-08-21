4920dba3a3c42ce00e1411d93cfbefd8386ffa11
Flowerska <mkwiatkowska@protonmail.com>
2024-08-21 20:42:16 +0200

    refactor database

--- PortAdapter/Mini/infrastructure/database.py
+++ PortAdapter/Mini/infrastructure/database.py
@@ -1,13 +1,28 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker, registry
 from adapters import orm
 
-engine = create_engine('sqlite:///:memory:', echo=False)
+class DB:
 
-metadata = MetaData()
-mapper_registry = registry()
+    def __init__(self, connection:str) -> None:
+        self.engine = create_engine(connection, echo=False)
+        self.metadata = MetaData()
+        self.mapper_registry = registry()
 
-user_table = orm.create_table(metadata=metadata)
-orm.map_table(mapper_registry, user_table=user_table)
+    def create_user_table(self) -> Table:
+        user_table = orm.create_table(metadata=self.metadata)
+        return user_table
+
+    def create_user_session(self):
+        user_table = self.create_user_table()
+        orm.map_table(self.mapper_registry, user_table=user_table)
+
+# engine = create_engine('sqlite:///:memory:', echo=False)
+
+# metadata = MetaData()
+# mapper_registry = registry()
+
+# user_table = orm.create_table(metadata=metadata)
+# orm.map_table(mapper_registry, user_table=user_table)
 
 SessionLocal = sessionmaker(bind=engine)
